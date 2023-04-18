ef844764c73bf1663175fdbcfcf84f11b9836bd8
Radhika <128808227+Verma-Radhika@users.noreply.github.com>
2023-04-18 17:48:16 +0530

    2ekhfjefwec

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app=express()
 app.use(express.json())
 const cors=require("cors")
 app.use(cors())
+const port =process.env.PORT || 4000
 
 app.get("/",(req,res)=>{
     res.stattus(200).json("hello")
 })
 
-app.listen(4000,()=>{
+app.listen(port,()=>{
     console.log("server started")
 })
