a64b8b58c4440c92ea261de3ca220e199c7a84c4
Radhika <128808227+Verma-Radhika@users.noreply.github.com>
2023-04-25 16:09:45 +0530

    23hqhj

--- index.js
+++ index.js
@@ -6,38 +6,8 @@ app.use(cors())
 const port =process.env.PORT || 4000
 
 app.get("/",(req,res)=>{
-    res.stattus(200).json("hello")
+  res.send("Hello")
 })
-
-const AWS = require('aws-sdk');
-
-// Set the region and credentials
-AWS.config.update({
-  accessKeyId: '<your-access-key-id>',
-  secretAccessKey: '<your-secret-access-key>',
-  region: '<your-region>'
-});
-
-// Create an AWS IoT client
-const iot = new AWS.Iot({
-  endpoint: ''
-});
-
-iot.subscribe({
-    topic: '<your-mqtt-topic>'
-  }, (err, data) => {
-    if (err) {
-      console.error('Failed to subscribe to topic:', err);
-    } else {
-      console.log('Subscribed to topic:', data);
-    }
-  });
-  
-  // Listen for incoming messages
-  iot.on('message', (topic, message) => {
-    console.log(`Received message on topic ${topic}: ${message}`);
-  });
-
 app.listen(port,()=>{
     console.log("server started")
 })
